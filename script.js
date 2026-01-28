90e7ed52dd7a0fc8d0efd512b988230d80ba28e1
SaiSharanya-16 <saisharanya310@gmail.com>
2026-01-28 12:01:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll(".section");
+
+const revealOnScroll = () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 120) {
+      section.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
